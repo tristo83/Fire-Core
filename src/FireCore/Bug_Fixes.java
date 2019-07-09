e3ddc514da6c6cc429e6638ab726b81ea7d68410
Tristan Carmichael <carmichael.tristan@gmail.com>
2019-07-09 19:08:24 +1000

    Bug Fixes
    - Removed getting info from database as this was worked out using info
    from jTable

--- src/FireCore/Bug_Fixes.java
+++ src/FireCore/Bug_Fixes.java
@@ -6,7 +6,6 @@
 //delete the current report and disply a new one
 //2. Finish working on the the email of the report
 //    - Need to be able to create mulitiple lines in the bodsy of the email
-//    - Take the info from the Database to display in the email report - 
 //3. Create a pdf report
 //4. Pop up dialog box to see if the user wants to clear the data in the table befor doing so
 //5. Add date a time search fiunction to the form
