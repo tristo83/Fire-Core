3054a06089cfd0f643b6dd75e2914efc2c46af00
Tristan Carmichael <carmichael.tristan@gmail.com>
2019-07-09 19:07:34 +1000

    Bug Fixes
    - Removed getting info from database as this was worked out using info
    from jTable

---
+++ src/FireCore/Bug_Fixes.java
@@ -0,0 +1,12 @@
+// BUG FIXES
+
+//DisplySearchData
+
+//1. Once the report has been run and the user hits the report button again ask them if they are sure that they want to
+//delete the current report and disply a new one
+//2. Finish working on the the email of the report
+//    - Need to be able to create mulitiple lines in the bodsy of the email
+//    - Take the info from the Database to display in the email report - 
+//3. Create a pdf report
+//4. Pop up dialog box to see if the user wants to clear the data in the table befor doing so
+//5. Add date a time search fiunction to the form
