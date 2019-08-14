d8f3ac47b9ad64e5e7f0eb78f66a19ca6afa64b9
Tristan Carmichael <carmichael.tristan@gmail.com>
2019-08-14 17:06:47 +1000

    Change import to: import static FireCore.SqlFunctions.host;

--- src/FireCore/SqlShowPanInfo.java
+++ src/FireCore/SqlShowPanInfo.java
@@ -7,7 +7,7 @@ package FireCore;
 
 import static FireCore.MainPage.jTree1;
 import static FireCore.SiteSetUpPage.mainSiteName;
-import static FireCore.SqlSiteSetUp.host;
+import static FireCore.SqlFunctions.host;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
