bd0833281cabb28ab0ac189110c38a27a8feba77
Tristan Carmichael <carmichael.tristan@gmail.com>
2019-08-14 17:06:16 +1000

    Added SqlUpdateSiteInfo function to SqlFunctions

--- src/FireCore/SqlFunctions.java
+++ src/FireCore/SqlFunctions.java
@@ -12,7 +12,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+import static FireCore.MainPage.jTree1;
+import static FireCore.SqlFunctions.host;
+import static java.lang.Integer.parseInt;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 
 
@@ -107,6 +117,88 @@ public class SqlFunctions {
     }
 
 
+public static void SqlUpdateSiteInfo(String bldNameInput, int sidNumInput, String ipAddress, int portNumInput, String manufacturer ) {
+
+        int panNumSetUp = parseInt(PanelSetUpPage.panelNumDisplay.getText());
+//        String mainSiteName = PanelSetUpPage.mainSiteNameInput.getText();
+        int id;
+        DefaultMutableTreeNode selectNode;
+
+        try {
+            Connection con = DriverManager.getConnection(host);
+            System.out.println("Connected");
+
+            String query = "SELECT * FROM db_accessadmin.Fire_Panel_Data";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                id = rs.getInt("panelID");
+
+                if (id == panNumSetUp) {
+
+                    String sql = "update db_accessadmin.Fire_Panel_Data set bldName = ?, sidNum = ?, portNum = ?, ipAddress = ?, manufacturer = ?  where panelID = ?";
+                    PreparedStatement st1 = con.prepareStatement(sql);
+
+                    st1.setString(1, bldNameInput);
+                    st1.setInt(2, sidNumInput);
+                    st1.setInt(3, portNumInput);
+                    st1.setString(4, ipAddress);
+                    st1.setString(5, manufacturer);
+                    st1.setInt(6, id);
+                    
+                    st1.executeUpdate();
+
+                    st1.close();
+                    st.close();
+                    con.close();
+                    
+                    
+                    
+
+                    if (bldNameInput.equals("")) {
+
+                        selectNode = (DefaultMutableTreeNode) jTree1.getSelectionPath().getLastPathComponent();
+
+                        selectNode.setUserObject("Panel " + panNumSetUp);
+
+                        DefaultTreeModel model = (DefaultTreeModel) jTree1.getModel();
+
+                        model.reload();
+                        
+                   
+                        
+        
+                        
+
+                    } else {
+                        selectNode = (DefaultMutableTreeNode) jTree1.getSelectionPath().getLastPathComponent();
+                        selectNode.setUserObject(bldNameInput);
+
+                        DefaultTreeModel model = (DefaultTreeModel) jTree1.getModel();
+
+                        model.reload();
+                        
+                        
+                        
+              
+ 
+                    }
+
+                    break;
+                }
+
+            }
+
+            for (int i = 0; i < jTree1.getRowCount(); i++) {
+                jTree1.expandRow(i);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        System.out.println("Connection Closed");
+
+    }
 
 
 
