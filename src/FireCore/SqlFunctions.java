79b1d5d43a92f27f0b37e283a8469cd43f51b2c3
Tristan Carmichael <carmichael.tristan@gmail.com>
2019-08-14 20:18:19 +1000

    Added function ShowPanInfo

--- src/FireCore/SqlFunctions.java
+++ src/FireCore/SqlFunctions.java
@@ -50,7 +50,15 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
-
+import static FireCore.MainPage.jTree1;
+import static FireCore.SiteSetUpPage.mainSiteName;
+import static FireCore.SqlFunctions.host;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.tree.TreeSelectionModel;
 
 /**
  *
@@ -420,6 +428,74 @@ public static void removePanel(int jTreeIndexNum) {
         System.out.println("Connection Closed");
 
     }
+public static void ShowPanInfo() {
 
+        TreeSelectionModel model = jTree1.getSelectionModel();
+
+        int test = model.getSelectionRows()[0];
+
+        test = test - 1;
+
+        try {
+
+            int id;
+            String sitename;
+            String bldName;
+            int sidNumber = 0;
+            int portNum;
+            String ipAddress;
+
+            Connection con = DriverManager.getConnection(host);
+            System.out.println("Connected");
+            String query = "SELECT * FROM db_accessadmin.Fire_Panel_Data";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                id = rs.getInt("panelID");
+
+                
+                
+
+                if (id == test) {
+
+                    System.out.println(id);
+                    sitename = rs.getString("mainSiteName");
+                    System.out.println(sitename);
+                    bldName = rs.getString("bldName");
+                    
+                    sidNumber = rs.getInt("sidNum");
+                    portNum = rs.getInt("portNum");
+                    ipAddress = rs.getString("ipAddress");
+                   
+                    PanelSetUpPage.mainSiteNameInput.setText(sitename);
+                    PanelSetUpPage.panelNumDisplay.setText(Integer.toString(id));
+                    PanelSetUpPage.bldNamFeild.setText(bldName);
+                    PanelSetUpPage.selectSidNum.setText(Integer.toString(sidNumber));
+                    PanelSetUpPage.PorttNumInput.setText(Integer.toString(portNum));
+                    PanelSetUpPage.ipAddressInput.setText(ipAddress);
+                    
+
+                    st.close();
+                    con.close();
+
+                    
+
+                    break;
+
+                } else if (test == 0) {
+
+                    break;
+                }
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
+        
+
+    }
 
 }
